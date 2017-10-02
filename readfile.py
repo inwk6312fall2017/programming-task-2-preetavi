728bb7b22149c5c086cdf86bcb19e744caa90202
preetavi <avinash.preetkaur@dal.ca>
2017-10-02 08:52:16 -0700

    appending words

--- readfile.py
+++ readfile.py
@@ -12,13 +12,13 @@ with open("crime.csv",'r') as file:
   if line[7] in mainlist:
    mainlist.index(line[7])
    count[index]+=1
+   list2[k]=count[index]
   else:
    list1.append(line[7])
    list2[i]=list[8]
    list2[j]=list[7]
    count.append(1)
-   index1=mainlist.index(
-   list2[k]=count[i]
+   list2[k]=count[index1]
    i+=3
    j+=3
    k+=3
