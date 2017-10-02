c082db9a4248e27eb55609aab9d5425446d1e4ec
preetavi <avinash.preetkaur@dal.ca>
2017-10-02 08:30:18 -0700

    appending words

--- readfile.py
+++ readfile.py
@@ -1,10 +1,17 @@
 with open("crime.csv",'r') as file:
  list1=[]
  list2=[]
- count=0 
-for line in file:
+ count=[]
+ i=0 
+ j=1
+ k=3
+ for line in file:
   line.strip()
   line.split(,)
   if line[7] in list1:
-  list1.append(line[7])
-  list2[0]=
+  
+  else:
+   list1.append(line[7])
+   list2[i]=list[8]
+   list2[j]=list[7]
+   list[k]=
