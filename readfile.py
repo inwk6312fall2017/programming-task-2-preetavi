9df43634d84882c40c4864719bb2fa43ed34911e
preetavi <avinash.preetkaur@dal.ca>
2017-10-02 08:23:39 -0700

    checking words

--- readfile.py
+++ readfile.py
@@ -1,5 +1,10 @@
 with open("crime.csv",'r') as file:
- for line in file:
- line.strip()
- line.split(,)
-
+ list1=[]
+ list2=[]
+ count=0 
+for line in file:
+  line.strip()
+  line.split(,)
+  if line[7] in list1:
+  list1.append(line[7])
+  list2[0]=
